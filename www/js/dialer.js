590582049cb66d9088bc4e35da5290505f8d7131
Admire Masaga <admiremasaga@gmail.com>
2017-07-16 08:03:09 +0200

    Update dialer.js

--- www/js/dialer.js
+++ www/js/dialer.js
@@ -1,10 +1,12 @@
 function dialNum(){
 window.plugins.CallNumber.callNumber(onSuccess, onError, "+263773384458", bypassAppChooser);
 function onSuccess(result){
+  alert ('calling');
   console.log("Success:"+result);
 }
 
 function onError(result) {
   console.log("Error:"+result);
+  alert ('calling');
 }
 }
