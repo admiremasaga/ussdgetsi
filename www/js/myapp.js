b290103bc2a2d2c7a5d19f78275dc45998af298f
Admire Masaga <admiremasaga@gmail.com>
2017-07-16 09:49:23 +0200

    Update myapp.js

--- www/js/myapp.js
+++ www/js/myapp.js
@@ -77,11 +77,16 @@ app.controller('formHide', function($scope){
 		
 		
 	}
-	
-	$scope.receiveSMS = function(){
-		receiveSMS();
-	}
 	$scope.dialNum = function(){
-		dialNum();
+	window.plugins.CallNumber.callNumber(onSuccess, onError, "0773384458", bypassAppChooser);
+function onSuccess(result){
+  alert ('calling');
+  console.log("Success:"+result);
+}
+
+function onError(result) {
+  console.log("Error:"+result);
+  alert ('calling');
+}
 	}
 });
